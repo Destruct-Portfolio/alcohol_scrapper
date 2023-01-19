d1c6cece77e52cf0865e96e40252ed6621421c22
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2023-01-19 15:28:48 +0100

    ....

--- src/components/wine_spirits.ts
+++ src/components/wine_spirits.ts
@@ -1,82 +1,102 @@
 import fs from "node:fs";
 import Hero_Scrapper from "./scrapper.js";
-
+import { product } from "../types/index.js";
 import Sleep from "../misc/sleep.js";
 
 export default class Wine_Spirits extends Hero_Scrapper {
     public static source = "https://shopredspirits.com/shop?category=all_spirits&category=all_exclud_sale_newarrivals&title=Explore"
 
-    public static payload: Set<string> = new Set()
-
-
     protected async $extract(): Promise<void> {
         await this.$bypass('https://shopredspirits.com/')
-        /*  await this.$client!.goto(Wine_Spirits.source,
-             { timeoutMs: 0 })
-         await this.$client!.waitForLoad('AllContentLoaded')
-         await Sleep.For(2)
-         fs.writeFileSync('t.jpeg', await this.$client!.takeScreenshot()) */
+        await this.$client!.goto(Wine_Spirits.source,
+            { timeoutMs: 0 })
+        await this.$client!.waitForLoad('AllContentLoaded')
+        await Sleep.For(2)
+
         let nextPage: boolean = true
         let numberOfElements = 0
 
-        /*      while (nextPage) {
-                 numberOfElements + 18
-                 try {
-                     console.log(await this.$client!.url)
-     
-                     let Links = await this.$client!.querySelectorAll('div.item-wrapper.product').$map(async (T) => {
-                         return await T.querySelector('a').href
-                     })
-     
-                     console.log(Links)
-                     if (Links.length === 0) {
-                         nextPage = false
-                     } else {
-     
-                         Links.map((link: string) => {
-                             this.$payload.push(link)
-                         })
-     
-                         numberOfElements = numberOfElements + 18
-     
-                         await this.$client!.goto(`https://shopredspirits.com/shop?category=all_spirits&category=all_exclud_sale_newarrivals&title=Explore&skip=${numberOfElements}`
-                             , { timeoutMs: 0 })
-     
-                         await this.$client!.waitForLoad('AllContentLoaded')
-     
-                     }
-                 } catch (error) {
-                     numberOfElements = numberOfElements - 18
-                 }
-     
-     
-             } */
+        while (nextPage) {
+            numberOfElements + 18
+            try {
+                console.log(await this.$client!.url)
 
-        let linksfe = [
-            'https://shopredspirits.com/shop?product-id=58585da739e21c3fb6d1289a&option-id=79afca4247d8f358889da55ab8cb72919b31983cb9a2767f8a75823e352f9eee',
-        ]
-        for (var i = 0; i < linksfe.length; i++) {
-            await this.$client!.goto(linksfe[i], { timeoutMs: 0 })
-            await this.$client!.waitForLoad("AllContentLoaded")
-            await Sleep.For(3)
-            await this.$client!.click(this.$client!.document.querySelector('button.age-check-yes'))
-            await Sleep.For(3)
-            /*  await this.$client!.scrollTo(this.$client!.document.querySelector('div.row.data-holder')!) */
+                let Links = await this.$client!.querySelectorAll('div.item-wrapper.product').$map(async (T) => {
+                    return await T.querySelector('a').href
+                })
 
-            fs.writeFileSync(`tt_${i}.jpeg`, await this.$client!.takeScreenshot())
-            let name = await this.$client!.document.querySelector('h1.name').innerText
-            console.log(name)
-            /*  let size = await this.$client!.document.querySelector('#product-element > ch-elements.product.page > div > div.ch-white-background > div.row.top-section > div.half.right-section > div:nth-child(1) > div.row.discount-price-container > ch-product-price-discount > div > div.product-price-discount-size-label.ch-primary-alt-color').innerText
-             console.log(size)
- 
-             let price = await this.$client!.document.querySelector('#product-element > ch-elements.product.page > div > div.ch-white-background > div.row.top-section > div.half.right-section > div:nth-child(1) > div.row.discount-price-container > ch-product-price-discount > div > div.product-price-discount-with').innerText
-             console.log(price)
- 
-             let prodcuctdet = await this.$client!.document.querySelector('#data-holder').innerText
-             console.log(prodcuctdet) */
-            let size = await this.$client!.xpathSelector('/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[3]/div[2]/div[1]/div[1]/ch-product-price-discount/div/div[1]/span').innerText
-            console.log(size)
+                console.log(Links)
+                if (Links.length === 0) {
+                    nextPage = false
+                } else {
+
+                    Links.map(async (link: string) => {
+                        let product = await this.SinglePage(link)
+                        if (product !== undefined) this.$payload.push(product)
+                        // handle the undi Product
+
+                    })
+
+                    numberOfElements = numberOfElements + 18
+
+                    await this.$client!.goto(`https://shopredspirits.com/shop?category=all_spirits&category=all_exclud_sale_newarrivals&title=Explore&skip=${numberOfElements}`
+                        , { timeoutMs: 0 })
+
+                    await this.$client!.waitForLoad('AllContentLoaded')
+
+                }
+            } catch (error) {
+                numberOfElements = numberOfElements - 18
+            }
+
+
         }
 
+
+    }
+
+    protected async SinglePage(url: string): Promise<product | undefined> {
+        try {
+            await this.$client!.goto(url, { timeoutMs: 0 })
+            await this.$client!.waitForLoad("AllContentLoaded")
+            await Sleep.For(3)
+            let name = this.$client!.xpathSelector(`/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[1]/div/h1`)
+            let size = this.$client!.xpathSelector('/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[3]/div[2]/div[1]/div[1]/ch-product-price-discount/div/div[1]/span')
+            let price = this.$client!.xpathSelector('/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[3]/div[2]/div[1]/div[1]/ch-product-price-discount/div/div[2]/span')
+            let type = this.$client!.xpathSelector(`/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[3]/div[2]/div[6]/div[2]/div[1]/div/span/span`)
+            let subtype = this.$client!.xpathSelector(`/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[3]/div[2]/div[6]/div[2]/div[2]/div`)
+            let country = this.$client!.xpathSelector(`//*[@id="data-holder"]/div[3]/div/span`)
+            let state = this.$client!.xpathSelector(`/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[3]/div[2]/div[6]/div[2]/div[4]/div/span`)
+            let pic = this.$client!.xpathSelector('/html/body/main/div/div/article/ch-elements.product.page/div/div[1]/div[3]/div[1]/div[3]/ch-product-image/div/figure[1]/img')
+
+            console.log({
+                name: await name.$exists ? await name.innerText : null,
+                size: await size.$exists ? await size.innerText : null,
+                price: await price.$exists ? await price.innerText : null,
+                type: await type.$exists ? await type.innerText : null,
+                subtype: await subtype.$exists ? await subtype.innerText : null,
+                country: await country.$exists ? await country.innerText : null,
+                state: await state.$exists ? await state.innerText : null,
+                pic: await pic.$exists ? await pic.src : null
+            })
+
+            return {
+                name: await name.$exists ? await name.innerText : null,
+                size: await size.$exists ? await size.innerText : null,
+                price: await price.$exists ? await price.innerText : null,
+                type: await type.$exists ? await type.innerText : null,
+                subtype: await subtype.$exists ? await subtype.innerText : null,
+                country: await country.$exists ? await country.innerText : null,
+                state: await state.$exists ? await state.innerText : null,
+                pic: await pic.$exists ? await pic.src : null
+            }
+
+
+        } catch (error) {
+            // needs to see if the error is a hero error 
+            // so i can restart the server
+            console.log(error)
+            return undefined
+        }
     }
 }
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+
+export interface product {
+    name: string | null;
+    size: string | null;
+    price: string | null;
+    type: string | null;
+    subtype: string | null;
+    country: string | null;
+    state: string | null;
+    pic: string | null;
+}
